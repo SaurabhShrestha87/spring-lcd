db98c470672ee375fcbca984237d99b15641b7e1
shileykumar <shileykumar@gmail.com>
2023-01-13 10:32:30 +0530

    Output changed x2

--- src/main/java/com/example/demo/controller/LcdController.java
+++ src/main/java/com/example/demo/controller/LcdController.java
@@ -11,15 +11,10 @@ import com.pi4j.io.gpio.RaspiPin;
 
 @RestController
 public class LcdController {
-
 	private GpioLcdDisplay lcd;
 	private GpioLcdDisplay lcd2;
-
-	private GpioController gpio;
-
 	public static final int LCD_ROWS = 2;
 	public static final int LCD_COLUMNS = 16;
-
 	private static final int LCD_ROW_1 = 0;
 	private static final int LCD_ROW_2 = 1;
 
@@ -29,7 +24,7 @@ public class LcdController {
 		modelAndView.setViewName("index");
 		try {
 			// Initialize the GPIO controller
-			gpio = GpioFactory.getInstance();
+			GpioController gpio = GpioFactory.getInstance();
 			// Initialize the LCD
 			lcd = new GpioLcdDisplay(LCD_ROWS, // Nr of rows
 					LCD_COLUMNS, // Nr of columns
@@ -41,22 +36,22 @@ public class LcdController {
 					RaspiPin.GPIO_03 // BCM 22: D7
 			);
 
-			lcd2 = new GpioLcdDisplay(LCD_ROWS, // Nr of rows
-					LCD_COLUMNS, // Nr of columns
-					RaspiPin.GPIO_06, // BCM 25: RS pin
-					RaspiPin.GPIO_21, // BCM 5: Strobe pin
-					RaspiPin.GPIO_04, // BCM 23: D4
-					RaspiPin.GPIO_00, // BCM 17: D5
-					RaspiPin.GPIO_01, // BCM 18: D6
-					RaspiPin.GPIO_03 // BCM 22: D7
-			);
+//			lcd2 = new GpioLcdDisplay(LCD_ROWS, // Nr of rows
+//					LCD_COLUMNS, // Nr of columns
+//					RaspiPin.GPIO_06, // BCM 25: RS pin
+//					RaspiPin.GPIO_21, // BCM 5: Strobe pin
+//					RaspiPin.GPIO_04, // BCM 23: D4
+//					RaspiPin.GPIO_00, // BCM 17: D5
+//					RaspiPin.GPIO_01, // BCM 18: D6
+//					RaspiPin.GPIO_03 // BCM 22: D7
+//			);
 			lcd.clear();
-			lcd2.clear();
+			//lcd2.clear();
 			lcd.write(0, "Loading LCD 1");
-			lcd2.write(0, "Loading LCD 2");
+			//lcd2.write(0, "Loading LCD 2");
 			for (int i = 0; i <= 100; i++) {
 				lcd.write(1, "..." + i + "%");
-				lcd2.write(1, "..." + i + "%");
+				//lcd2.write(1, "..." + i + "%");
 				Thread.sleep(50);
 			}
 			// Initial output to check if the wiring is OK
@@ -79,9 +74,9 @@ public class LcdController {
 	@PostMapping
 	public void toggleScreen2(@RequestBody String payload) {
 		if(payload.equalsIgnoreCase("true")){
-			lcdOutput(lcd2, "LCD 2", "Switch ON");
+			//lcdOutput(lcd2, "LCD 2", "Switch ON");
 		}else{
-			lcdOutput(lcd2, "LCD 2", "Switch OFF");
+			//lcdOutput(lcd2, "LCD 2", "Switch OFF");
 		}
 	}
 	@RequestMapping(value = "/toggle3", consumes = "text/plain")
@@ -95,7 +90,7 @@ public class LcdController {
 	public void lcdOutput(GpioLcdDisplay lcd, String text, String text2) {
 		try {
 			lcd.clear();
-			lcd.write(LCD_ROW_1, text, LCDTextAlignment.ALIGN_CENTER);
+			lcd.write(LCD_ROW_1                       , text, LCDTextAlignment.ALIGN_CENTER);
 			lcd.write(LCD_ROW_2, text2, LCDTextAlignment.ALIGN_CENTER);
 		} catch (Exception ex) {
 			System.err.println("Error: " + ex.getMessage());
