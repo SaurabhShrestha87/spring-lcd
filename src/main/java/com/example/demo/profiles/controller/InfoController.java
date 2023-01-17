23c188c1204f1370d0a7ea6c0755f29d81e8d861
shileykumar <shileykumar@gmail.com>
2023-01-17 15:19:07 +0530

    Added file upload fix #2

--- src/main/java/com/example/demo/profiles/controller/InfoController.java
+++ src/main/java/com/example/demo/profiles/controller/InfoController.java
@@ -91,7 +91,7 @@ public class InfoController {
 				file.transferTo( new File("D:\\upload\\" + fileName));
 			}
 			else{
-				file.transferTo( new File("root\\home\\pi\\Pictures\\" + fileName));
+				file.transferTo( new File("/home/pi/Application/Uploads/" + fileName));
 			}
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
