b22760e756f1917fb7cf2b1128a2a46549c529ec
shileykumar <shileykumar@gmail.com>
2023-01-17 14:32:00 +0530

    Added file upload

--- src/main/java/com/example/demo/profiles/controller/InfoController.java
+++ src/main/java/com/example/demo/profiles/controller/InfoController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pi4j.component.lcd.LCDTextAlignment;
@@ -23,6 +24,7 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.RaspiPin;
 
+import java.io.File;
 import java.util.List;
 
 @RestController
@@ -73,6 +75,30 @@ public class InfoController {
 		return modelAndView;
 	}
 
+	@GetMapping("/upload")
+	public ModelAndView imageUpload() {
+//		init();
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("upload");
+		return modelAndView;
+	}
+
+	@PostMapping("/upload")
+	public ResponseEntity<?> handleFileUpload( @RequestParam("file") MultipartFile file ) {
+		String fileName = file.getOriginalFilename();
+		try {
+			if(OSValidator.isWindows()){
+				file.transferTo( new File("D:\\upload\\" + fileName));
+			}
+			else{
+				file.transferTo( new File("home\\pi\\Pictures\\" + fileName));
+			}
+			file.transferTo( new File("D:\\upload\\" + fileName));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.ok("File uploaded successfully.");
+	}
 	private void LedInit() {
 		try {
 			System.out.println("Starting SPI example...");
