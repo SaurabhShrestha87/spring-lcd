c34680c9c511144a31f60acb482109e9222092f4
saurabhshrestha <shresthasaurabh86@gmail.com>
2023-02-10 15:54:50 +0530

    SerialCommunication Gif method changes #9

--- src/test/java/com/example/demo/TestClass.java
+++ src/test/java/com/example/demo/TestClass.java
@@ -2,20 +2,16 @@ package com.example.demo;
 
 import com.example.demo.model.DeviceType;
 import com.example.demo.service.SerialCommunication;
-import com.example.demo.utils.FileUtils;
 import com.example.demo.utils.GifDecoder;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.image.BufferedImage;
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.demo.utils.FileUtils.readBufferedData;
 import static com.example.demo.utils.GifDecoder.BufferedImageFrame;
 
 public class TestClass {
