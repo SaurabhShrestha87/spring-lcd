9123fd511bb0de3f25821811ec857d1867df52f4
saurabhshrestha <shresthasaurabh86@gmail.com>
2023-02-13 17:12:28 +0530

    VideoDecoder extractFrames() fix #1

--- src/main/java/com/example/demo/utils/RunShellCommandFromJava.java
+++ src/main/java/com/example/demo/utils/RunShellCommandFromJava.java
@@ -6,7 +6,7 @@ import com.example.demo.model.Panel;
 import com.example.demo.service.SerialCommunication;
 import lombok.SneakyThrows;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -80,37 +80,19 @@ public class RunShellCommandFromJava {
     }
 
     public class VideoDecoder {
-        public void extractFrames(String videoFilePath) {
-            try (FrameGrabber grabber = new FFmpegFrameGrabber(videoFilePath)) {
-                try (Java2DFrameConverter converter = new Java2DFrameConverter()) {
-                    try {
-                        grabber.start();
-                        long frameRate = (long) grabber.getFrameRate();
-                        int i = 0;
-                        while (true) {
-                            BufferedImage frame = converter.convert(grabber.grab());
-                            if (frame == null) {
-                                System.out.println(" null FRAME : " + i);
-                                break;
-                            }
-                            // Save the frame to a file
-                            // ImageIO.write(frame, "png", new File("frame" + i + ".png"));
-                            try {
-                                serialCommunication.runSerial(FileUtils.asInputStream(frame));
-                                System.out.println("FRAME : " + i + " AT FRAME RATE : " + frameRate);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                            i++;
-                        }
-                        grabber.stop();
-                    } catch (Exception e) {
-                        logger.error("extractFrames() Error : " + e);
-                    }
-                }
-            } catch (FrameGrabber.Exception e) {
-                logger.error("extractFrames() Error : " + e);
+        public void extractFrames(String videoFilePath) throws IOException {
+            FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(videoFilePath);
+            frameGrabber.start();
+            int frameNumber = 0;
+            Frame frame;
+            Java2DFrameConverter frameConverter = new Java2DFrameConverter();
+            while ((frame = frameGrabber.grab()) != null) {
+                frameNumber++;
+                BufferedImage bufferedImage = frameConverter.getBufferedImage(frame);
+                serialCommunication.runSerial(FileUtils.asInputStream(bufferedImage));
+                System.out.println("FRAME : " + frameNumber);
             }
+            frameGrabber.stop();
         }
     }
 
--- src/test/java/com/example/demo/TestClass.java
+++ src/test/java/com/example/demo/TestClass.java
@@ -4,12 +4,14 @@ import com.example.demo.model.DeviceType;
 import com.example.demo.service.SerialCommunication;
 import com.example.demo.utils.FileUtils;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -20,6 +22,22 @@ public class TestClass {
 
     }
 
+    @Test
+    public void readFileTest2() throws IOException {
+        SerialCommunication serialCommunication = new SerialCommunication(DeviceType.DEVICE0);
+        FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber("D:\\upload\\video.mp4");
+        frameGrabber.start();
+        int frameNumber = 0;
+        Frame frame;
+        Java2DFrameConverter frameConverter = new Java2DFrameConverter();
+        while ((frame = frameGrabber.grab()) != null) {
+            frameNumber++;
+            BufferedImage bufferedImage = frameConverter.getBufferedImage(frame);
+            serialCommunication.runSerial(FileUtils.asInputStream(bufferedImage));
+            System.out.println("FRAME : " + frameNumber);
+        }
+        frameGrabber.stop();
+    }
     @Test
     public void readFileTest() {
         SerialCommunication serialCommunication = new SerialCommunication(DeviceType.DEVICE0);
