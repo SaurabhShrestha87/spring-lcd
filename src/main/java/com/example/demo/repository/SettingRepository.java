a5fee77aa6c65a19840fdc2112bf79029269bd1b
saurabh <shresthasaurabh86@gmail.com>
2023-03-22 12:58:31 +0530

    Make sure to update database #2. setting and panel_config table has to be added and data inserted.

--- src/main/java/com/example/demo/repository/SettingRepository.java
+++ src/main/java/com/example/demo/repository/SettingRepository.java
@@ -1,9 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Profile;
-import com.example.demo.model.Setting;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.example.demo.model.setting.Setting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
